3f31731dc221916c6ddac2cb96713aa26fbca4ad
wilomaku <wjgarciah@unal.edu.co>
2017-12-06 09:17:54 -0200

    New files

--- dev/bib_mri/compute_angles.py
+++ dev/bib_mri/compute_angles.py
@@ -5,14 +5,20 @@ def compute_angles(pivot, anterior, posterior):
     max_angle = np.pi*2
 
     def angles(vectors):
-        return np.arctan2(vectors[0], vectors[1])
+        return np.arctan2(vectors[1], vectors[0])
 
     ap, pp = anterior-pivot, posterior-pivot
-
     ang_post, ang_ant = angles(pp), angles(ap)
     ang = ang_post - ang_ant
-    idx = ang<-(3*np.pi/2.0)
-    ang[idx] = (ang[idx]+(2*np.pi))
-    idx = ang<0.75
-    ang[idx] += max_angle
-    return 360 - ang*180/(np.pi)
+
+    dif_prof = np.abs(ang - np.roll(ang,1)) > np.pi
+    ind_start = np.where(dif_prof)[0][::2]
+    ind_end = np.where(dif_prof)[0][1::2]
+    zeros = np.zeros_like(ang)
+    for in1, in2 in zip(ind_start,ind_end):
+	if (ang[in1] - np.roll(ang,1)[in1]) > np.pi:
+	    zeros[in1:in2] = -2*np.pi
+	else:
+	    zeros[in1:in2] = 2*np.pi
+
+    return (ang + zeros) *180/(np.pi)
